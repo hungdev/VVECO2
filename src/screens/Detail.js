71bb1784256f9ea966ac1a59a2aec9bd7aa6d127
Cee <hungns126@gmail.com>
2022-03-30 19:31:06 +0700

    fix

--- src/screens/Detail.js
+++ src/screens/Detail.js
@@ -1,7 +1,7 @@
 import { Button, View, Text } from 'react-native';
 import React from 'react'
 
-export default function Detail() {
+export default function Detail({ navigation }) {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Details Screen</Text>
