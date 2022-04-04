e5591466086d0cc3615516f6580708c3f59b182b
Cee <hungns126@gmail.com>
2022-04-04 20:53:39 +0700

    add redux persit

--- App.js
+++ App.js
@@ -1,32 +1,23 @@
 import { View, Text } from "react-native";
 import React from "react";
 //Redux
-import { createStore } from "redux";
+import { createStore, combineReducers } from "redux";
 import { Provider } from "react-redux";
-
 import { persistStore, persistReducer } from 'redux-persist'
 import { PersistGate } from 'redux-persist/integration/react'
-import AsyncStorage from '@react-native-async-storage/async-storage';
 
-// import allReducer from "./src/reducers";
 import Navigation from "./src/navigation";
 
 import { store } from './src/store/store'
 
-const persistConfig = {
-  key: 'root',
-  storage: AsyncStorage,
-  // blacklist: ['userReducer']
-}
-
 export default function Detail() {
-  // const persistedReducer = persistReducer(persistConfig, allReducer)
-  // const store = createStore(persistedReducer);
-  // let persistor = persistStore(store)
+  let persistor = persistStore(store)
 
   return (
     <Provider store={store}>
-      <Navigation />
+      <PersistGate loading={null} persistor={persistor}>
+        <Navigation />
+      </PersistGate>
     </Provider>
   );
 }
--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,17 @@
 import { configureStore } from '@reduxjs/toolkit'
+import { createStore, combineReducers } from "redux";
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { persistStore, persistReducer } from 'redux-persist'
 import HomeSlice from './HomeSlice'
 
-export const store = configureStore({
-  reducer: {
-    product: HomeSlice
-  },
-})
+export const allReducers = combineReducers({
+  product: HomeSlice
+});
+
+const persistConfig = {
+  key: 'root',
+  storage: AsyncStorage
+};
+
+const persistedReducer = persistReducer(persistConfig, allReducers);
+export const store = createStore(persistedReducer);
