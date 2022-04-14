0820ef6c572976d188d6807f8e43e6a5bc44bcf9
Cee <hungns126@gmail.com>
2022-04-14 20:39:39 +0700

    fix warning

--- App.js
+++ App.js
@@ -37,9 +37,10 @@ const store = configureStore({
   reducer: persistedReducer,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-      },
+      // serializableCheck: {
+      //   ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      // },
+      serializableCheck: false,
     }),
 });
 
